87645ff82e856d4c23b224cca5c79e31f1810c39
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-07-05 14:51:07 -0500

    update package and use bash. depends on npm > 6

--- build.py
+++ build.py
@@ -1,15 +1,13 @@
 import os
 from subprocess import Popen, PIPE, check_output
 
-def powershell(cmd):
-    return check_output(f'powershell.exe -c "{cmd}"').decode('latin-1')
-
 def bash(cmd):
-    return check_output(f'bash -c "{cmd}"').decode('latin-1')
+    return check_output(['bash', '-c', cmd]).decode('latin-1')
 
 def main():
     os.chdir('client')
-    print(powershell("npm run-script build"))
+    # print(bash('pwd'))
+    print(bash("npm run-script build"))
     os.chdir('..')
     print(bash('rm -rf static'))
     print(bash('cp -r client/build/static .'))
